999eb3c76261eb4e36d07b8ee28acbac071c58b1
Admin <patilsharad2000@gmail.co>
2021-07-20 12:58:14 +0530

    prime number

--- src/BasicProgram/PrimeNo.java
+++ src/BasicProgram/PrimeNo.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class PrimeNo {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Number......");
-        int no=scanner.nextInt();
-
-        if (no%2 == 0){
-            System.out.println("Not Prime");
-        }
-        else {
-            System.out.println("Prime");
+        int i,m=0,flag=0;
+        int n=2;//it is the number to be checked
+        m=n/2;
+        if(n==0||n==1){
+            System.out.println(n+" is not prime number");
+        }else{
+            for(i=2;i<=m;i++){
+                if(n%i==0){
+                    System.out.println(n+" is not prime number");
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)  { System.out.println(n+" is prime number"); }
         }
     }
 }
